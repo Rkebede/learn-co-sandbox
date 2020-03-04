a46fdfc108527de1cea738e9776e756c065c1883
Ruth Kebede <rutht.kebede@gmail.com>
2020-03-04 16:57:49 +0000

    Done.

---
+++ case_statements.rb
@@ -0,0 +1,18 @@
+case greeting = "friendly_greeting"
+  when "unfriendly_greeting"
+    puts "What do you want"
+  when "friendly_greeting"
+    puts "Hi!"
+end
+
+
+
+
+current_weather = "raining"
+
+case current_weather
+  when "sunny"
+    puts "grab some sunscreen!"
+  when "raining"
+    puts "grab an umbrella"
+end
---
+++ code_challenge.rb
@@ -0,0 +1,11 @@
+number = 102
+
+def returning_false (number)
+  if 7 > 4 && 100 < (number)
+    puts true
+  else 
+    puts false
+  end
+end
+
+returning_false (number)
