65b1f638e825087e4f0ce8dd665ff969bf9c95f6
Ruth Kebede <rutht.kebede@gmail.com>
2020-03-05 04:23:23 +0000

    Done.

---
+++ iterators_intro.rb
@@ -0,0 +1,35 @@
+# input = ""
+# while input != "Mommm!!"
+#   puts "Stop hitting youself!"
+#   input = gets.chomp
+# end
+
+# brothers = ["Tom", "Tim", "Jim"]
+
+# count = 0
+# while count <= brothers.length-1
+#   name = brothers[count]
+#   puts "Stop hitting youself #{name}!"
+#   count += 1 
+# end
+
+
+brothers = ["Tom", "Tim", "Jim"]
+
+#array is being iterated over (it's taking each element and providing a reference to it). 
+# The element #{name} is being referenced
+
+# brothers.each do |name|
+
+# output response with element 
+
+#   puts "Stop hitting youself #{name}."
+# end
+
+
+
+friends = ["jane", "jim", "john"]
+
+friends.each do |friend|
+  puts "Hello #{friend}!"
+end 
