d244c04dcb1706c20a862b285509aad9d93350b2
Ruth Kebede <rutht.kebede@gmail.com>
2020-03-05 02:27:28 +0000

    Done.

---
+++ arrays.rb
@@ -0,0 +1,6 @@
+
+students = ["Harry Potter", "Ron Weasley", "Hermione Granger", "Draco Malfoy"]
+
+#to call a student 
+students[0]
+  "Harry Potter"
---
+++ iteration_abstraction.rb
@@ -0,0 +1,15 @@
+basket = ["apple 1", "apple 2", "apple 3","apple 4","apple 5","apple 6","apple 7","apple 8","apple 9","apple 10"]
+
+apples_in_basket = basket.size 
+apples_taken_out = 0 
+
+loop do 
+  if apples_taken_out < apples_in_basket
+    #taking out apple 1 
+    puts "Taking out #{basket[apples_taken_out]}"
+
+    apples_taken_out += 1 
+  else
+    break
+  end
+end
